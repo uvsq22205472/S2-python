8dd046868495a787b5cf9cd4eeae0d6038797cff
probablyzora <oliwier.szmyt@ens.uvsq.fr>
2023-03-21 10:16:35 +0100

    ...

--- TD/TD2/dessin.py
+++ TD/TD2/dessin.py
@@ -7,11 +7,16 @@ ROOT.title("Mon dessin")
 Canvas_Height = 500
 Canvas_Width = 500
 Canvas = tk.Canvas(ROOT,bg="black",height=Canvas_Height,width=Canvas_Width)
+ChosenColor = "blue"
+
+def RandomizeCoordinates():
+    global x,y
+    x,y = randint(0,Canvas_Width-50),randint(0,Canvas_Height-50)
+    return x,y
 
 def CreateRandomCircleInSpace():
-    x,y = 0,0
-    x,y = randint(0,500),randint(0,500)
-    Canvas.create_oval((x,y),(x+50,y+50),fill="blue" ,width=5)
+    RandomizeCoordinates()
+    Canvas.create_oval((x,y),(x+50,y+50),fill="blue",width=2)
 
 Button1 = tk.Button(ROOT,text="Choisir une couleur", font=("helvetica", "12"), relief="groove")
 Button1.grid(column=6,row=0)
@@ -19,11 +24,19 @@ Button1.grid(column=6,row=0)
 Button2 = tk.Button(ROOT,text="Cercle", font=("helvetica", "12"), relief="groove",command=CreateRandomCircleInSpace)
 Button2.grid(column=0,row=3,rowspan=3)
 
+def CreateRandomSquareInSpace():
+    RandomizeCoordinates()
+    Canvas.create_rectangle((x,y),(x+50,y+50),fill="blue",width=2)
 
-Button3 = tk.Button(ROOT,text="Carré", font=("helvetica", "12"), relief="groove")
+Button3 = tk.Button(ROOT,text="Carré", font=("helvetica", "12"), relief="groove", command=CreateRandomSquareInSpace)
 Button3.grid(column=0,row=9,rowspan=3)
 
-Button4 = tk.Button(ROOT,text="Croix", font=("helvetica", "12"), relief="groove")
+def CreateRandomCrossInSpace():
+    RandomizeCoordinates()
+    Canvas.create_rectangle((x,y),(x+50,y+50),width=1,outline="green")
+    Canvas.create_line((x,y),(x+50,y+50),width=3,fill="yellow")
+    Canvas.create_line((x+50,y),(x,y+50),width=3,fill="yellow")
+Button4 = tk.Button(ROOT,text="Croix", font=("helvetica", "12"), relief="groove", command=CreateRandomCrossInSpace)
 Button4.grid(column=0,row=16,rowspan=3)
 
 Canvas.grid(column=1,row=1,columnspan=20,rowspan=20)
